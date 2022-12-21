c6e4551d4f3127fb511cd9108476c57f9089964e
Sybe <118173520+SybeTech@users.noreply.github.com>
2022-12-21 10:49:35 +0100

    Add files via upload

---
+++ Round Numbers.py
@@ -0,0 +1,15 @@
+# How to round a number. 
+
+number = 3.14159265359
+
+round_number = round(number)
+print (round_number)
+
+# round (number, digits)
+
+round_number = round(number, 2)
+print (round_number)
+
+round_number = round(number, 4)
+print (round_number)
+
